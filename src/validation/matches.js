70bd4f24d57e0ad229e18c64c19f16a85e30e3dc
iakash-singh <akashsingh7202@gmail.com>
2026-02-23 23:17:31 +0530

    implement the web socket server

--- src/validation/matches.js
+++ src/validation/matches.js
@@ -22,6 +22,7 @@ export const createMatchSchema = z
     homeTeam: z.string().min(1),
     awayTeam: z.string().min(1),
     startTime: z.iso.datetime(),
+    endTime: z.iso.datetime(),
     homeScore: z.coerce.number().int().nonnegative().optional(),
     awayScore: z.coerce.number().int().nonnegative().optional(),
   })
